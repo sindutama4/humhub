0703939178f50d6e1f1e80ae48d3103174d595f2
sindutama4 <sindutama4@gmx.com>
2017-01-19 21:21:56 +0700

    Create error.php

---
+++ protected/humhub/messages/su/error.php
@@ -0,0 +1,7 @@
+// Sundanese (Basa Sunda) Translation by sindutama4@gmx.com
+<?php
+return array (
+  '<strong>Login</strong> required' => '<strong>Login</strong> diperlukeun',
+  'An internal server error occurred.' => 'Aya kasalahan server internal.',
+  'You are not allowed to perform this action.' => 'Anjeun teu gaduh izin ngalakukeun aksi ieu.',
+);
