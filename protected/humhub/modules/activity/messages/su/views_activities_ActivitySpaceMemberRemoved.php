849c4364df660f22581d57fb575e137791eb8e47
sindutama4 <sindutama4@gmx.com>
2017-01-20 09:18:55 +0700

    Create views_activities_ActivitySpaceMemberRemoved.php

---
+++ protected/humhub/modules/activity/messages/su/views_activities_ActivitySpaceMemberRemoved.php
@@ -0,0 +1,7 @@
+// Sundanese (Basa Sunda) Translation by sindutama4@gmx.com
+
+<?php
+return array (
+  '%displayName% left the space %spaceName%' => '%displayName% ningalkeun ruang %spaceName%',
+  '%displayName% left this space.' => '%displayName% ningalkeun ruang ieu.',
+);
