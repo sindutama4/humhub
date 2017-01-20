c5e92de976132f736406cb1124e5ce77c9f964c2
sindutama4 <sindutama4@gmx.com>
2017-01-20 09:15:27 +0700

    Create widgets_views_activityStream.php

---
+++ protected/humhub/modules/activity/messages/su/widgets_views_activityStream.php
@@ -0,0 +1,7 @@
+// Sundanese (Basa Sunda) Translation by sindutama4@gmx.com
+
+<?php
+return array (
+  '<strong>Latest</strong> activities' => 'Aktipitas <strong>Teranyar</strong>',
+  'There are no activities yet.' => 'Teu acan aya aktipitas',
+);
