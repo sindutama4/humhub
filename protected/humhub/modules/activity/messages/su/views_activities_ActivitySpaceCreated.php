e3d747ae76009870a68150eda549ce4b0fce7f17
sindutama4 <sindutama4@gmx.com>
2017-01-20 09:20:39 +0700

    Create views_activities_ActivitySpaceCreated.php

---
+++ protected/humhub/modules/activity/messages/su/views_activities_ActivitySpaceCreated.php
@@ -0,0 +1,7 @@
+// Sundanese (Basa Sunda) Translation by sindutama4@gmx.com
+
+<?php
+return array (
+  '%displayName% created the new space %spaceName%' => '%displayName% nyieun ruang anyar %spaceName%',
+  '%displayName% created this space.' => '%displayName% nyieun ruang ieu.',
+);
