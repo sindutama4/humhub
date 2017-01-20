25e6998967442efbcedf2e5149fd6c6b161d400a
sindutama4 <sindutama4@gmx.com>
2017-01-20 09:20:21 +0700

    Create views_activities_ActivitySpaceMemberAdded.php

---
+++ protected/humhub/modules/activity/messages/su/views_activities_ActivitySpaceMemberAdded.php
@@ -0,0 +1,7 @@
+// Sundanese (Basa Sunda) Translation by sindutama4@gmx.com
+
+<?php
+return array (
+  '%displayName% joined the space %spaceName%' => '%displayName% gabung di ruang %spaceName%',
+  '%displayName% joined this space.' => '%displayName% gabung di ruang ieu.',
+);
